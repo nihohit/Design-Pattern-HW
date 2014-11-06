4c7663820612e5b7d6e7dbe790af56d4d0b4d717
kkerenor <kkeren.or@gmail.com>
2014-11-06 18:39:26 +0200

    start work on console

--- Ex00/Ex00.GarageManagementSystem.ConsoleUI/Program.cs
+++ Ex00/Ex00.GarageManagementSystem.ConsoleUI/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Ex00.GarageManagementSystem.ConsoleUI
+﻿using System;
+
+namespace Ex00.GarageManagementSystem.ConsoleUI
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            
         }
     }
 }
