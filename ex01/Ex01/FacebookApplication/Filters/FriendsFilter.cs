c248669ebb878594d882532c2bc2a8364d3e89c8
Shachar <nihohit@gmail.com>
2014-12-19 13:51:48 +0200

    Moved filters to separate directory

---
+++ ex01/Ex01/FacebookApplication/Filters/FriendsFilter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FacebookApplication.Interfaces;
+using FacebookWrapper.ObjectModel;
+
+namespace FacebookApplication
+{
+    public class FriendsFilter : IFriendFilter
+    {
+        #region members
+
+        private readonly Dictionary<string, User> r_FilteredFriends;
+
+        #endregion members
+        #region Properties
+        #region IFriendFilter
+
+        public string Name { get; private set; }
+
+        public IEnumerable<string> FilterdFriendsIds
+        {
+            get { return r_FilteredFriends.Keys; }
+        }
+
+        public IEnumerable<User> FilterdFriends
+        {
+            get { return r_FilteredFriends.Values; }
+        }
+
+        public IEnumerable<IUsersFilter> UserFilters { get; private set; }
+
+        public string ErrorString { get; private set; }
+
+        #endregion IFriendFilter
+        #endregion Properties
+        #region constructor
+
+        public FriendsFilter(string i_Name, IEnumerable<IUsersFilter> i_UserFilters)
+        {
+            Name = i_Name;
+            UserFilters = i_UserFilters;
+            r_FilteredFriends = new Dictionary<string, User>();
+        }
+        #endregion constructor
+        #region public methods
+
+        public void UpdateFriends(IEnumerable<User> i_Friends)
+        {
+            r_FilteredFriends.Clear();
+            ErrorString = string.Empty;
+            IEnumerable<User> friends = i_Friends;
+            if (i_Friends != null && i_Friends.Count() > 1)
+            {
+                foreach (IUsersFilter filter in UserFilters)
+                {
+                    Dictionary<string, FacebookObjectCollection<User>>
+                        friendsThatThrowExceptionWhenTriedToFilterByErrorMessage;
+                    friends = filter.FilterUsers(friends, out friendsThatThrowExceptionWhenTriedToFilterByErrorMessage);
+                    if (friendsThatThrowExceptionWhenTriedToFilterByErrorMessage != null)
+                    {
+                        foreach (string errorMessage in friendsThatThrowExceptionWhenTriedToFilterByErrorMessage.Keys)
+                        {
+                            ErrorString += string.Format(
+                                "{0} couldn't be filtered because: {1}{2}",
+                                string.Join(", ", friendsThatThrowExceptionWhenTriedToFilterByErrorMessage[errorMessage].Select(i_User => i_User.Name)),
+                                errorMessage,
+                                Environment.NewLine);
+                        }
+                    }
+                }
+            }
+
+            foreach (User friend in friends)
+            {
+                r_FilteredFriends.Add(friend.Id, friend);
+            }
+
+            ErrorString = ErrorString.Trim(Environment.NewLine.ToCharArray());
+        }
+
+        public override string ToString()
+        {
+            string displayString = string.Format("'{0}': ", Name);
+            foreach (IUsersFilter userFilter in UserFilters)
+            {
+                displayString += userFilter + ", ";
+            }
+
+            return displayString.Trim().Trim(',');
+        }
+        #endregion public methods
+    }
+}
---
+++ ex01/Ex01/FacebookApplication/Filters/UsersFriendListsFilter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FacebookApplication.Interfaces;
+using FacebookWrapper.ObjectModel;
+
+namespace FacebookApplication
+{
+    public class UsersFriendListsFilter : BaseUserFilter
+    {
+        #region members
+
+        private readonly IFriendListsManager r_FriendListsManager;
+        #endregion members
+
+        #region Properties
+
+        public FriendList FriendListBelongsTo { get; private set; }
+
+        #endregion Properties
+
+        #region constructor
+
+        public UsersFriendListsFilter(FriendList i_FriendList, IFriendListsManager i_FriendListsManager)
+        {
+            r_FriendListsManager = i_FriendListsManager;
+            FriendListBelongsTo = i_FriendList;
+        }
+
+        #endregion constructor
+
+        #region override methods
+
+        protected override bool MantianConstrain(User i_User)
+        {
+            IEnumerable<string> frindBelongToListsIds =
+                r_FriendListsManager.GetAllFriendListsWhichFriendBelongsTo(i_User.Id);
+
+            return frindBelongToListsIds.Contains(FriendListBelongsTo.Id);
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("Belongs to {0}", FriendListBelongsTo.Name);
+        }
+        #endregion 
+    }
+}
